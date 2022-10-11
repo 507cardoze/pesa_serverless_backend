724b3b1f3054e6072402cc5c8053579c7aee279d
507cardoze <anthonycardoze@gmail.com>
2022-10-11 02:51:25 -0500

    :sparkles: (adding authorizer with idtoken firebase)

---
+++ src/functions/authorizer/handler.ts
@@ -0,0 +1,43 @@
+import { generateIamPolicy, initializeSdk } from '@libs/firebase';
+import { middyfy } from '@libs/lambda';
+import admin from 'firebase-admin';
+import { APIGatewayTokenAuthorizerHandler, AuthResponse } from 'aws-lambda';
+import { getAuthToken } from '@libs/get-authtoken';
+
+const authorizer: APIGatewayTokenAuthorizerHandler = async (
+	event
+): Promise<AuthResponse> => {
+	try {
+		const isOffline = process.env.IS_OFFLINE ?? '';
+
+		if (isOffline)
+			return generateIamPolicy('Allow', event.methodArn, {
+				uid: '123',
+				email: 'example@gmail.com',
+				role: 'admin',
+				aud: '',
+				auth_time: 0,
+				exp: 0,
+				firebase: {
+					identities: {},
+					sign_in_provider: '',
+					sign_in_second_factor: '',
+					second_factor_identifier: '',
+					tenant: '',
+				},
+				iat: 0,
+				iss: '',
+				sub: '',
+			});
+
+		const bearerToken = getAuthToken(event.authorizationToken);
+		if (!bearerToken) return generateIamPolicy('Deny', event.methodArn, null);
+		initializeSdk();
+		const decodedData = await admin.auth().verifyIdToken(bearerToken);
+		return generateIamPolicy('Allow', event.methodArn, decodedData);
+	} catch (err) {
+		return generateIamPolicy('Deny', event.methodArn, null);
+	}
+};
+
+export const main = middyfy(authorizer);
--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,1 +1,2 @@
-export { default as hello } from './hello';
+export { default as me } from './me';
+export { default as authorizer } from './authorizer';
