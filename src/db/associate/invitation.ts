87a9da27fbfba013b5662c4735f94fb8457e6fd6
507cardoze <anthonycardoze@gmail.com>
2022-10-12 16:53:43 -0500

    :bug: (fix relationship in invitation)

--- src/db/associate/invitation.ts
+++ src/db/associate/invitation.ts
@@ -1,22 +1,12 @@
 import { db } from '@db/db';
 
 export const invitationAssociation = async (DB: db) => {
-	await DB.player.belongsToMany(DB.team, {
-		through: DB.invitation,
-		foreignKey: 'playerId',
-	});
-
 	await DB.player.hasMany(DB.invitation, {
 		foreignKey: 'playerId',
 	});
 
 	await DB.invitation.belongsTo(DB.player, {
 		foreignKey: 'playerId',
-	});
-
-	await DB.team.belongsToMany(DB.player, {
-		through: DB.invitation,
-		foreignKey: 'teamId',
 	});
 
 	await DB.team.hasMany(DB.invitation, {
